5e9d5e5bf852c054e31d51812669168b66b8f7db
Lukas Folkman <lukas.folkman@gmail.com>
2025-06-04 18:18:24 +1000

    log time for inference + nms

--- ultralytics/models/yolo/detect/predict.py
+++ ultralytics/models/yolo/detect/predict.py
@@ -20,9 +20,33 @@ class DetectionPredictor(BasePredictor):
         ```
     """
 
-    def postprocess(self, preds, img, orig_imgs):
+    # ORIGINAL
+    # def postprocess(self, preds, img, orig_imgs):
+    #     """Post-processes predictions and returns a list of Results objects."""
+    #     nc = ((preds[0] if isinstance(preds, (list, tuple)) else preds).shape[1] - 4)
+    #     preds = ops.non_max_suppression(
+    #         preds,
+    #         self.args.conf,
+    #         self.args.iou,
+    #         agnostic=self.args.agnostic_nms,
+    #         max_det=self.args.max_det,
+    #         classes=self.args.classes,
+    #         include_cls_probs=True
+    #     )
+    #
+    #     if not isinstance(orig_imgs, list):  # input images are a torch.Tensor, not a list
+    #         orig_imgs = ops.convert_torch2numpy_batch(orig_imgs)
+    #
+    #     results = []
+    #     for pred, orig_img, img_path in zip(preds, orig_imgs, self.batch[0]):
+    #         pred[:, :4] = ops.scale_boxes(img.shape[2:], pred[:, :4], orig_img.shape)
+    #         results.append(Results(orig_img, path=img_path, names=self.model.names,
+    #                                boxes=pred[:, :-nc], box_cls_probs=pred[:, -nc:]))
+    #     return results
+
+    # NEW
+    def postprocess(self, preds):
         """Post-processes predictions and returns a list of Results objects."""
-        nc = ((preds[0] if isinstance(preds, (list, tuple)) else preds).shape[1] - 4)
         preds = ops.non_max_suppression(
             preds,
             self.args.conf,
@@ -33,12 +57,4 @@ class DetectionPredictor(BasePredictor):
             include_cls_probs=True
         )
 
-        if not isinstance(orig_imgs, list):  # input images are a torch.Tensor, not a list
-            orig_imgs = ops.convert_torch2numpy_batch(orig_imgs)
-
-        results = []
-        for pred, orig_img, img_path in zip(preds, orig_imgs, self.batch[0]):
-            pred[:, :4] = ops.scale_boxes(img.shape[2:], pred[:, :4], orig_img.shape)
-            results.append(Results(orig_img, path=img_path, names=self.model.names,
-                                   boxes=pred[:, :-nc], box_cls_probs=pred[:, -nc:]))
-        return results
+        return preds
